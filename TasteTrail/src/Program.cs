8844037e5c82c977db8b7db957bbc7946e97befa
makedoniaz <jafar.sadig04@gmail.com>
2024-07-28 14:40:52 +0400

    Add default swagger routing

--- TasteTrail/src/Program.cs
+++ TasteTrail/src/Program.cs
@@ -14,7 +14,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
