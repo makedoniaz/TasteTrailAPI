6afe51ffa7443eb72230f1bbe8ba2a23eea06052
makedoniaz <jafar.sadig04@gmail.com>
2024-07-28 12:48:26 +0400

    Create project

---
+++ TasteTrail/src/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Test() {
+            return Ok(1);
+        }
+    }
+}
